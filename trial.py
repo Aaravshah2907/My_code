29b489aa609575eac8c03da3dd07922d3141e62d
Aaravshah2907 <aaravshah0011@gmail.com>
2023-07-04 11:03:58 +0530

    Update

--- trial.py
+++ trial.py
@@ -1,129 +1,33 @@
-"""
-import random
+from Encrypter_Programs import encrypter2
 
-text_original = ' ₹abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-=_+[];,./\{}|:"<>?'
-text_copy = list(text_original)
-text_final = ""
-while len(text_original) != len(text_final):
-    choice = random.randint(0, len(text_copy) - 1)
-    t = text_copy[choice]
-    text_copy.remove(t)
-    text_final += t
-
-print(text_final)
-"""
 main = [
-    " ",
-    "!",
-    '"',
-    "#",
-    "$",
-    "%",
-    "&",
-    "'",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    ";",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "[",
-    "\\",
-    "]",
-    "^",
-    "_",
-    "`",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "{",
-    "|",
-    "}",
-    "~",
-    "₹",
-    "'",
+    " ",    "!",    '"',    "#",    "$",    "%",    "&",    "'",    "(",    ")",    "*",    "+",
+    ",",    "-",    ".",    "/",    "0",    "1",    "2",    "3",    "4",    "5",    "6",    "7",
+    "8",    "9",    ";",    "<",    "=",    ">",    "?",    "@",    "A",    "B",    "C",    "D",
+    "E",    "F",    "G",    "H",    "I",    "J",    "K",    "L",    "M",    "N",    "O",    "P",
+    "Q",    "R",    "S",    "T",    "U",    "V",    "W",    "X",    "Y",    "Z",    "[",    "\\",
+    "]",    "^",    "_",    "`",    "a",    "b",    "c",    "d",    "e",    "f",    "g",    "h",
+    "i",    "j",    "k",    "l",    "m",    "n",    "o",    "p",    "q",    "r",    "s",    "t",
+    "u",    "v",    "w",    "x",    "y",    "z",    "{",    "|",    "}",    "~",    "₹",    ":",
 ]
 
-s = input("String:")
-print(len(s))
-f = []
-for _ in sorted(s):
-    if _ in f:
-        pass
-    else:
-        f.append(_)
+text_original = ' ₹abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-=_+[];,./\{}|:"<>?\''
 
-print(f)
-print(len(f))
-for element in main:
-    if element in f:
-        pass
+key = 1111111111111111
+
+def func(s):
+    print(len(s))
+    f = []
+    for element in s:
+        if element not in main:
+            f.append(element)
+            
+    if f:
+        print(f)
     else:
-        print(element)
+        print("Successful")
+
+for x in range(0,10):
+    output = encrypter2.main(0,text_original,str(int(key*x)))
+    print("Iteration :", x)
+    func(output)
