619167013f755a75753bbb8697e6ff0976f98bd2
Aaravshah2907 <aaravshah0011@gmail.com>
2023-08-16 22:24:07 +0530

    Commit Update

--- GUI_apps/Learning/combobox_spinbox.py
+++ GUI_apps/Learning/combobox_spinbox.py
@@ -7,6 +7,39 @@ window.title("Buttons")
 window.geometry('600x500')
 
 # Combobox
+items = ['icecream','pizza','pasta']
+food_str = tk.StringVar(value = 'Default Value')
+cmbbos = ttk.Combobox(window,textvariable=food_str)
+cmbbos['values'] = items
+# cmbbos.config(values=items)
+# cmbbos.configure(values=items)
+cmbbos.pack()
+
+# events for Combobox
+cmbbos.bind('<<ComboboxSelected>>', lambda event: print(f'{food_str.get()} was selected'))
+# cmbbos_label = ttk.Label(window, text=food_str.get())
+# By defualt the second bind function will only execute.
+cmbbos.bind('<<ComboboxSelected>>', lambda event: cmbbos_label.configure(text=f'{food_str.get()} was selected'))
+cmbbos_label = ttk.Label(window, text='A trial')
+cmbbos_label.pack()
+
+# Spinbox
+
+list_values = [0,1,2,3,4,5,6,7,8,9]
+trial_str = tk.StringVar(value = list_values[5])
+spnbox = ttk.Spinbox(window,textvariable=trial_str, command = lambda : print("Arrow pressed"))
+spnbox['values'] = list_values
+# spnbox.config(values=items)
+# spnbox.configure(values=items)
+spnbox.pack()
+# event: spnbox_label.config(text=trial_str.get())
+# events for Combobox
+spnbox.bind('<<Increment>>', lambda event: print(f'{trial_str.get()} was selected'))
+# spnbox_label = ttk.Label(window, text=food_str.get())
+# By defualt the second bind function will only execute.
+spnbox.bind('<<Decrement>>', lambda event: spnbox_label.configure(text=f'{trial_str.get()} was selected'))
+spnbox_label = ttk.Label(window, text = 'A trial')
+spnbox_label.pack()
 
 # run
 window.mainloop()
