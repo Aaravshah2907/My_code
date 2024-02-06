a6fa8fffc489f7ffeb6b645bec394559b8108e8f
Aarav Shah <aaravshah0011@gmail.com>
2024-02-06 11:16:54 +0530

    Update

--- Encrypter_Programs/mix_2_converter.py
+++ Encrypter_Programs/mix_2_converter.py
@@ -8,7 +8,7 @@ password = input("Enter Password: ")
 key = input("Key: ")
 hash = encrypter2.main(0, password, key)
 
-with open("database.csv", 'r') as file:
+with open("database1.csv", 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         database_list.append({ "username": row['username'] , "password": row['password'], "hc":row["hash_code"], "key":row["key"]})
@@ -18,6 +18,6 @@ database_list.append({'username':username, "password":password, "hc":hash, 'key'
 for element in database_list:
     database_dict[element['username']] = [element['key'], element['password'] , element['hc']]
 
-with open('database.csv', 'a', newline='') as file:
+with open('database1.csv', 'a', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=['username','key','password',"hash_code"])
     writer.writerow({'username': username, 'key': key,'password': password, "hash_code": hash})
