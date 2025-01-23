2a74de2c4d1f000f4973a041e32dea7eb53a4f5e
Aarav <aaravshah0011@gmail.com>
2025-01-23 14:42:17 +0530

    -

--- Youtube (Audio,Video) Downloader/Songs.Downloader.Youtube.py
+++ Youtube (Audio,Video) Downloader/Songs.Downloader.Youtube.py
@@ -8,31 +8,15 @@ print("Select download format:")
 print("1: Video file with audio (.mp4)")
 print("2: Audio only (.mp3)")
 media_type = input()
-path1 = r'D:\Videos'
-path2 = r'D:\Music'
+path1 = r'~/Documents/Personal/Yt-Videos/'
+path2 = r'~/Documents/Personal/AudioBooks/'
 path_exist1 = os.path.exists(path1)
 path_exist2 = os.path.exists(path2)
 
-
-if media_type == '1':
-    if path_exist1:
-        pass
-    else:
-        os.mkdir(path1)
-
-elif media_type == '2':
-    if path_exist2:
-        pass
-    else:
-        os.mkdir(path2)
-else:
-    pass
-
 if media_type == "1":
     video = yt.streams.get_by_resolution('720p')
-    name = yt.title.replace("/", ".")
-    name = name.replace("\\", ".").replace("|", ".").replace("*", ".").replace(
-        "?", ".").replace('"', ".").replace('<', ".").replace('>', ".").replace(':', ".")
+    name = yt.title()
+    # name = name.replace("\\", ".").replace("|", ".").replace("*", ".").replace( "?", ".").replace('"', ".").replace('<', ".").replace('>', ".").replace(':', ".")
 
     video.download(output_path=path1, filename=name+'.mp4')
     size = video.filesize/1048576
@@ -41,9 +25,8 @@ if media_type == "1":
 elif media_type == "2":
     video = yt.streams.filter(only_audio=True).first()
     size = video.filesize_approx/1048576
-    name = yt.title.replace("/", ".")
-    name = name.replace("\\", ".").replace("|", ".").replace("*", ".").replace(
-        "?", ".").replace('"', ".").replace('<', ".").replace('>', ".").replace(':', ".")
+    name = yt.title()
+    #name = name.replace("\\", ".").replace("|", ".").replace("*", ".").replace("?", ".").replace('"', ".").replace('<', ".").replace('>', ".").replace(':', ".")
 
     video.download(output_path=path2, filename=name+"."+yt.author+".mp3")
     print(f'{yt.title} by {yt.author} downloaded in {size} mb')
