ed2263734ff1b3d51e0d82806d0db9763dd7c8c4
Aarav <aaravshah0011@gmail.com>
2026-01-28 12:50:49 +0530

    Updated Yt Download Script

---
+++ Youtube (Audio,Video) Downloader/withCoverImage.py
@@ -0,0 +1,73 @@
+import yt_dlp
+import os
+import sys
+
+def sanitize_filename(name):
+    invalid_chars = ['\\', '|', '*', '?', '"', '<', '>', ':', '/']
+    for char in invalid_chars:
+        name = name.replace(char, '.')
+    return name.strip()
+
+def expand_path(path):
+    return os.path.expanduser(path)
+
+def download_youtube(url, media_type):
+    path_video = expand_path('~/Documents/Personal/Yt-Videos/')
+    path_audio = expand_path('~/Documents/Personal/AudioBooks/')
+    os.makedirs(path_video, exist_ok=True)
+    os.makedirs(path_audio, exist_ok=True)
+
+    ydl_opts = {}
+    if media_type == "1":  # Video with audio
+        ydl_opts = {
+            'format': 'bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'outtmpl': os.path.join(path_video, '%(title)s.%(ext)s'),
+            'merge_output_format': 'mp4',
+        }
+    elif media_type == "2":  # Audio only mp3 with THUMBNAIL
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': os.path.join(path_audio, '%(title)s.%(ext)s'),
+            'writethumbnail': True,  # Downloads the thumbnail image file
+            'postprocessors': [
+                {
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                },
+                {
+                    'key': 'EmbedThumbnail',  # Embeds the thumbnail into the MP3 metadata
+                },
+            ],
+        }
+    else:
+        print("Invalid media type selection.")
+        return
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        print(f'Download complete: {sanitize_filename(info["title"])}')
+
+def main():
+    if len(sys.argv) > 3:
+        print("Usage: python Songs.Downloader.Youtube.py [<YouTube URL> <media type>]")
+        return
+    
+    if len(sys.argv) > 1 and sys.argv[1] in ['-h', '--help', '-help', 'help', '-?', '-man']:
+        print("Usage: python Songs.Downloader.Youtube.py [<YouTube URL> <media type>]")
+        return
+    
+    url_sysarg = sys.argv[1] if len(sys.argv) > 1 else None
+    if url_sysarg:
+        url = url_sysarg
+        media_type = sys.argv[2] if len(sys.argv) > 2 else "1"
+    else:
+        url = input("Enter the YouTube video URL:\n>> ").strip()
+        print("Select download format:")
+        print("1: Video file with audio (.mp4)")
+        print("2: Audio only (.mp3)")
+        media_type = input(">> ").strip()
+    download_youtube(url, media_type)
+
+if __name__ == "__main__":
+    main()
